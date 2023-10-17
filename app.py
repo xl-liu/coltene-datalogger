1a02b040a571805329db8408be54e8473d7692a0
Xintong Liu <xintongliu211@gmail.com>
2023-10-18 01:37:09 +0200

    fixed missing displays and added service to run datalogger at start-up

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask, render_template, request, send_file, send_from_director
 import time
 import csv
 import os
-import threading
+from threading import Thread
 import smbus
 from rtd_lib import tempADC
-from pijuice import PiJuice
+from pijuice_lib import PiJuice
+from datetime import datetime 
 
 # ... (previous code)
 
@@ -14,7 +15,7 @@ recording = False  # Flag to control recording
 
 # I2C setup
 bus = smbus.SMBus(1)
-temp_adc = tempADC(bus)
+temp_adc = tempADC(bus, n_channels=5)
 pijuice = PiJuice(bus)
 
 # CSV file setup
@@ -22,9 +23,11 @@ downloads_directory = "logdata"
 
 def get_pi_status():
     pi_temp = os.popen("vcgencmd measure_temp").readline()
-    pi_temp = pi_temp.replace("temp=", "").strip()
+    # pi_temp = pi_temp.replace("temp=", "").strip()
+    pi_temp = pi_temp[5:-3]
     pi_time = time.strftime("%Y-%m-%d %H:%M:%S")
-    return pi_temp, pi_time
+    pi_space = get_available_space()
+    return pi_temp, pi_time, pi_space
 
 def get_battery_status():
 
@@ -45,11 +48,25 @@ def get_battery_status():
         battery_faults = []
         for fault, flag in faults_info['data'].items():
             if flag: battery_faults.append(fault)
+        if not battery_faults:
+            battery_faults = 'None'
     else:
         battery_faults = 'N/A'
 
     return battery_temp, battery_power, battery_faults
 
+def get_available_space():
+    try:
+        stat = os.statvfs('/')  # Replace with the mount point of /dev/root if different
+        # Calculate available space
+        available_space = stat.f_frsize * stat.f_bavail
+        # Convert to human-readable format (bytes to GB)
+        available_space_gb = available_space / (1024 ** 3)
+        return round(available_space_gb, 2)
+    except Exception as e:
+        print(f'Error getting disk space: {e}')
+        return 'N/A'
+    
 @app.route("/")
 def index():
     # List all files in the downloads directory
@@ -59,43 +76,57 @@ def index():
 
 @app.route("/status")
 def status():
-    temperature, timestamp = get_pi_status()
+    temperature, timestamp, avail_space = get_pi_status()
     battery_temp, battery_power, battery_faults = get_battery_status()
-    return jsonify({ 
+    dic = { 
         "temperature": temperature,
         "timestamp": timestamp,
+        "available_space": avail_space,
         "recording": recording,  # Pass recording status to the client
         "battery_temp": battery_temp,
         "battery_power": battery_power,
         "battery_faults": battery_faults
-        })
+        }
+    # print(dic)
+    return jsonify(dic)
 
 @app.route("/download/<filename>")
 def download_file(filename):
     # Serve files from the downloads directory
     return send_from_directory(downloads_directory, filename)
 
+def record_data():
+    global recording
+    while True:
+        if recording:
+            now = datetime.now()
+            timestamp = now.strftime('%H:%M:%S.%f')[:-3]
+            # Replace the following line with your I2C reading function
+            temps = temp_adc.read_all_channels()
+            
+            with open(csv_filename, "a", newline="") as csvfile:
+                csv_writer = csv.writer(csvfile)
+                csv_writer.writerow([timestamp] + temps)
+        time.sleep(0.2)  # Sleep for 200ms to achieve ~5Hz
+
 @app.route("/start_stop_recording", methods=["POST"])
 def start_stop_recording():
     global recording
+    global csv_filename
     if recording:
         recording = False
-        csv_filename = time.strftime("%Y-%m-%d_%H-%M-%S") + '.csv'
-        filename = downloads_directory + '/' + csv_filename
-        with open(filename, "w", newline="") as csvfile:
-            csv_writer = csv.writer(csvfile)
-            csv_writer.writerow(["Time", "Data"])  # Add appropriate headers
-            # Read data from I2C here
-            # data = bus.read_byte_data(address, register)  # Replace with your I2C read code
-            data = 1
-            timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
-            csv_writer.writerow([timestamp, data])
     else:
         recording = True
+        csv_filename = f"{downloads_directory}/{time.strftime('%Y-%m-%d_%H-%M-%S')}.csv"
+        with open(csv_filename, "w", newline="") as csvfile:
+            csv_writer = csv.writer(csvfile)
+            csv_writer.writerow(["Time", "Temperature Ch0","Temperature Ch1","Temperature Ch2","Temperature Ch3","Temperature Ch4", "Pressure Data"])  # Add appropriate headers
     return jsonify({"recording": recording})
 
 if __name__ == "__main__":
-    
+    record_thread = Thread(target=record_data)
+    record_thread.start()
+
     app.debug = True
     app.run(host="0.0.0.0", port=70)
 
--- read_sensors.py
+++ read_sensors.py
@@ -1,6 +1,7 @@
 from ups_lib import INA219
 from rtd_lib import tempADC
-from pijuice import PiJuice
+from pijuice_lib import PiJuice
+import os 
 
 import smbus
 bus = smbus.SMBus(1)    # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
@@ -9,23 +10,31 @@ temp_adc = tempADC(bus)
 # ina219 = INA219(bus)
 pijuice = PiJuice(bus)
 
-# start recording 
-
-
 # get pressure sensor reading
 
 # get temperature sensor reading
 print('temperature')
 print(temp_adc.read_all_channels())
 
-# get pressure sensor reading
-
 # get pijuice status reading
 print('battery')
 print(pijuice.status.GetChargeLevel())
 
-# write to file
+# read storage space
+def get_available_space():
+    try:
+        stat = os.statvfs('/')  # Replace with the mount point of /dev/root if different
+        # Calculate available space
+        available_space = stat.f_frsize * stat.f_bavail
+        # Convert to human-readable format (bytes to GB)
+        available_space_gb = available_space / (1024 ** 3)
+        return f"{available_space_gb:.2f}G"
+    except Exception as e:
+        print(f"Error getting disk space: {e}")
+        return "N/A"
 
+print('storage')
+print(get_available_space())
 
 # # get waveshare ups status
 # bus_voltage = ina219.getBusVoltage_V()             # voltage on V- (load side)
